b4bf907a3b15261b573067061c9a8689e17c1da8
Aryan Rastogi <rastogi.a@etoncollege.org.uk>
2019-11-05 11:10:50 +0000

    Initial checking of code

---
+++ StandardAlgorithms/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  StandardAlgorithms
+//
+//  Created by Rastogi, Aryan (WING) on 05/11/2019.
+//  Copyright © 2019 Rastogi, Aryan (WING). All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let name = "ARYAN"
+print(name)
