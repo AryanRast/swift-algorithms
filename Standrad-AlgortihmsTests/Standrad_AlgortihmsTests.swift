73adfba7cbb72b6f2780a5616ed19d5c83b1c80e
Aryan Rastogi <rastogi.a@etoncollege.org.uk>
2019-11-05 11:22:23 +0000

    adding test target

---
+++ Standrad-AlgortihmsTests/Standrad_AlgortihmsTests.swift
@@ -0,0 +1,33 @@
+//
+//  Standrad_AlgortihmsTests.swift
+//  Standrad-AlgortihmsTests
+//
+//  Created by Rastogi, Aryan (WING) on 05/11/2019.
+//  Copyright © 2019 Rastogi, Aryan (WING). All rights reserved.
+//
+
+import XCTest
+
+class Standrad_AlgortihmsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
